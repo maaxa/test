c9a00635306b3f386b816bedd1bf699611b6b2ab
test test <test@yandex.ru>
2019-12-01 20:36:47 +0300

    Updates

---
+++ precache-manifest.cac38e7c4dcf3684bce01ef1a0e5b6f3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9ed823a2c96545daf71df584756cfc8a",
+    "url": "/react-app/index.html"
+  },
+  {
+    "revision": "19466b8551736ca1b21b",
+    "url": "/react-app/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "76d2b646144b338764b0",
+    "url": "/react-app/static/js/2.ac732591.chunk.js"
+  },
+  {
+    "revision": "19466b8551736ca1b21b",
+    "url": "/react-app/static/js/main.4b228206.chunk.js"
+  },
+  {
+    "revision": "a92bcfe37307eec876e2",
+    "url": "/react-app/static/js/runtime-main.b959d2f6.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/react-app/static/media/logo.25bf045c.svg"
+  }
+]);
